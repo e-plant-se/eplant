60b9fbbeadc93fb6d296fc27fa1932756778ebee
Brandon Sandoval <colima_97@hotmail.com>
2019-07-13 16:55:51 -0500

    Changed strings that are showed into spanish, added a button for Sign in

--- eplant/eplant.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ eplant/eplant.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "b6fb7f97-2875-423b-b961-22c7ce3ea64c",
+		"XAMARIN_BUILD_ID", "b5a5ed23-e8db-434a-aefc-75fb222308c5",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
