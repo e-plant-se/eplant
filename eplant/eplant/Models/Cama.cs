abc1adb8a928977323002bc2227f4a80354fb3cd
LuisJOrtiz <ljoo259@gmail.com>
2019-07-13 19:22:45 -0500

    agregué los botones y puse los datos de la lista en 2 columnas (creo), falta prueba

--- eplant/eplant/Models/Cama.cs
+++ eplant/eplant/Models/Cama.cs
@@ -11,7 +11,7 @@ namespace eplant.Models
         public string tipo { get; set; }
         public string estructura { get; set; }
         public string planta1 { get; set; }
-        //public string planta2 { get; set; }
+        public string planta2 { get; set; }
         public string imageUrl { get; set; }
         public string fechaCultivo { get; set; }
 
@@ -19,10 +19,10 @@ namespace eplant.Models
         {
             List<Cama> camas = new List<Cama>()
             {
-                new Cama(){ huerto="Cocoteros", idCama=1, tipo="Dedicado", estructura="Simple", planta1="Jitomates", imageUrl="camaVacia.png", fechaCultivo="02-07-2019"},
-                new Cama(){ huerto="Cocoteros", idCama=2, tipo="Dedicado", estructura="Simple", planta1="Chile", imageUrl="camaVacia.png", fechaCultivo="02-07-2019"},
-                new Cama(){ huerto="Cocoteros", idCama=3, tipo="Dedicado", estructura="Simple", planta1="Berenjena", imageUrl="camaVacia.png", fechaCultivo="02-07-2019"},
-                new Cama(){ huerto="Cocoteros", idCama=4, tipo="Dedicado", estructura="Simple", planta1="Papa", imageUrl="camaVacia.png", fechaCultivo="02-07-2019"}
+                new Cama(){ huerto="Cocoteros", idCama=1, tipo="Dedicado", estructura="Simple", planta1="Jitomates", planta2="", imageUrl="camaVacia.png", fechaCultivo="02-07-2019"},
+                new Cama(){ huerto="Cocoteros", idCama=2, tipo="Dedicado", estructura="Simple", planta1="Chile", planta2="", imageUrl="camaVacia.png", fechaCultivo="02-07-2019"},
+                new Cama(){ huerto="Cocoteros", idCama=3, tipo="Dedicado", estructura="Simple", planta1="Berenjena", planta2="", imageUrl="camaVacia.png", fechaCultivo="02-07-2019"},
+                new Cama(){ huerto="Cocoteros", idCama=4, tipo="Dedicado", estructura="Simple", planta1="Papa", planta2="", imageUrl="camaVacia.png", fechaCultivo="02-07-2019"}
             };
             return camas;
         }
